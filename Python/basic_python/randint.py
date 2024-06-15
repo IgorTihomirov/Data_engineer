946af800d434bf536abcf2e29416f24935b6c362
Igor <i.n.tihomirov@yandex.ru>
2024-06-15 17:01:59 +0300

    change files: randint.py

--- Python/basic_python/randint.py
+++ Python/basic_python/randint.py
@@ -3,9 +3,9 @@
 from random import randint as rnd
 # Объявить функцию get_dumplings_recommendation(),
 # которая вернёт (return) случайное число в диапазоне от 10 до 20.
-def get_dumplings_recommendation():
-    quantity = rnd(10, 20)
+def get_dumplings_recommendation(min,max):
+    quantity = rnd(min,max)
     return quantity
 # Вызвать функцию get_dumplings_recommendation() и напечатать заданную фразу.
-quantity_dumplings = get_dumplings_recommendation()
+quantity_dumplings = get_dumplings_recommendation(20, 30)
 print('Оптимальным количеством пельменей на сегодня будет', quantity_dumplings)
