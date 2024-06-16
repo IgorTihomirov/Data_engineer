a64c16e75e018ba91c95bb26a513e1382923cf9f
Igor <i.n.tihomirov@yandex.ru>
2024-06-16 15:43:41 +0300

    change files: if_else_month.py

--- Python/basic_python/if_else_month.py
+++ Python/basic_python/if_else_month.py
@@ -4,29 +4,13 @@
 
 # Место для вашего кода
 def get_season(name_month):
-    if name_month == 'январь':
+    if name_month == 'январь' or name_month == 'февраль' or name_month == 'декабрь':
         return 'зима'
-    elif name_month == 'февраль':
-        return 'зима'
-    elif name_month == 'декабрь':
-        return 'зима'
-    elif name_month == 'март':
+    elif name_month == 'март' or name_month == 'апрель' or name_month == 'май':
         return 'весна'
-    elif name_month == 'апрель':
-        return 'весна'
-    elif name_month == 'май':
-        return 'весна'
-    elif name_month == 'июнь':
+    elif name_month == 'июнь' or name_month == 'июль' or name_month == 'август':
         return 'лето'
-    elif name_month == 'июль':
-        return 'лето'
-    elif name_month == 'август':
-        return 'лето'
-    elif name_month == 'сентябрь':
-        return 'осень'
-    elif name_month == 'октябрь':
-        return 'осень'
-    elif name_month == 'ноябрь':
+    elif name_month == 'сентябрь' or name_month == 'октябрь' or name_month == 'ноябрь':
         return 'осень'
     else:
         return 'Ошибка в написании месяца!'
