a0dbe93c8c57660d9288f501ff2f28e2c9f537e8
Igor <i.n.tihomirov@yandex.ru>
2024-06-18 22:47:44 +0300

    new files: Unpacking_the_list.py

---
+++ Python/basic_python/Unpacking_the_list.py
@@ -0,0 +1,11 @@
+any_sequence = [1, 2, 3]
+# Список можно распаковать в набор переменных.
+# Имена переменных задаёт разработчик.
+first, second, third = any_sequence
+
+# Проверим, что получилось:
+print(first)
+print(second)
+print(third)
+# А что со списком?
+print(any_sequence)
