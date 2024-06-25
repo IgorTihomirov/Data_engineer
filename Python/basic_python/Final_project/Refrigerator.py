2635bf19ae5efcb49be21ce05454321e4e56bf34
Igor <i.n.tihomirov@yandex.ru>
2024-06-25 23:59:20 +0300

    change files: Refrigerator.py add function add_by_note

--- Python/basic_python/Final_project/Refrigerator.py
+++ Python/basic_python/Final_project/Refrigerator.py
@@ -1,13 +1,7 @@
 import datetime
 from decimal import Decimal
 
-goods = {
-    'Пельмени Универсальные': [
-        {'amount': Decimal('0.5'), 'expiration_date': datetime.date(2023, 7, 15)},
-        {'amount': Decimal('2'), 'expiration_date': datetime.date(2023, 8, 1)}
-    ],
-    'Вода': [{'amount': Decimal('1.5'), 'expiration_date': None}]
-}
+goods = {}
 DATE_FORMAT = '%Y-%m-%d'
 
 def add(items, title, amount, expiration_date=None):
@@ -21,14 +15,25 @@ def add(items, title, amount, expiration_date=None):
             items[title] = []  # то создать пустой список для этого товара.
         items[title].append({'amount': amount, 'expiration_date': date.date()}) #Добавить в список items[title] словарь, содержащий информацию о количестве товара good_amount и дате истечения expiration_date, если он был предоставлен
 
-add(goods, 'Яйца', Decimal('10'), '2023-9-30')
-print(goods)
 
 
 def add_by_note(items, note):
-    ...
+    parts = note.split() # разделяем строку на части
+    last_part = [-1] # последняя часть строки
+    pre_last_part = [-2] # предпоследняя часть строки
+    if last_part == DATE_FORMAT: # Проверка последней части строки. Если она представляет дату в формате "год-месяц-день"
+        expiration_date = datetime.datetime.strptime(last_part, DATE_FORMAT) #то она считается датой и сохраняется в переменную expiration_date
+        amount = pre_last_part # предпоследняя часть строки считается количеством товара и сохраняется в переменную amount
+        title = ' '.join(parts[:-2]) # все остальные части объединяются в название товара и сохраняются в переменную title
+    else: #Проверка последней части строки. Если она не представляет датой
+        amount = last_part # то она считается количеством товара и сохраняется в переменную amount
+        title = ' '.join(parts[:-1])  # все остальные части объединяются в название товара и сохраняются в переменную title
+        expiration_date = None # переменная expiration_date устанавливается в None
+    add(items, title, amount, expiration_date=None) # Вызов функции add с извлеченными данными (название товара, количество, дата истечения) для добавления их в список items
 
 
+add_by_note(goods, 'Яйца гусиные 4 2023-07-15')
+print(goods)
 def find(items, needle):
     ...
 
