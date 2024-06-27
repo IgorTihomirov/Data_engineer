e46b27912c31b0c662500eaf6718a55bb8299caf
Igor <i.n.tihomirov@yandex.ru>
2024-06-27 20:44:31 +0300

    change files: Refrigerator.py add function find() - worked add function amount() - worked

--- Python/basic_python/Final_project/Refrigerator.py
+++ Python/basic_python/Final_project/Refrigerator.py
@@ -2,9 +2,12 @@ import datetime
 from decimal import Decimal
 
 goods = {
-    'Яйца': [{'amount': Decimal('1'), 'expiration_date': datetime.date(2023, 6, 24)}],
-    'Яйца гусиные': [{'amount': Decimal('4'), 'expiration_date': datetime.date(2023, 7, 15)}],
-    'Морковь': [{'amount': Decimal('2'), 'expiration_date': datetime.date(2023, 8, 6)}]
+    'Яйца': [{'amount': Decimal('1'), 'expiration_date': None}],
+    'Морковь': [
+        {'amount': Decimal('2'), 'expiration_date': datetime.date(2023, 8, 1)},
+        {'amount': Decimal('3'), 'expiration_date': datetime.date(2023, 8, 6)}
+    ],
+    'Вода': [{'amount': Decimal('2.5'), 'expiration_date': None}]
 }
 DATE_FORMAT = '%Y-%m-%d'
 
@@ -16,7 +19,10 @@ def add(items, title, amount, expiration_date=None):
         expiration_date,
         DATE_FORMAT
     ).date() if expiration_date else expiration_date
-    items[title].append({'amount': amount, 'expiration_date': expiration_date})
+    list.append(
+        items[title],
+{'amount': amount, 'expiration_date': expiration_date}
+    )
 
 
 def add_by_note(items, note):
@@ -25,24 +31,32 @@ def add_by_note(items, note):
         add(items, ' '.join(parts[:-2]), Decimal(parts[-2]), parts[-1])
     else:
         add(items, ' '.join(parts[:-1]), Decimal(parts[-1]))
-
 #print(goods)
 
 
 def find(items, needle):
     result = []
     for title in items:
-        if needle in str.lower(title):
-            result = list.append(result, needle)
-            return result
+        if needle.lower() in title.lower():
+            result.append(title)
+    return result
 
 
-print(find(goods, 'йц'))
+#print(find(goods, 'йц'))
 
 
 def amount(items, needle):
-    ...
+    product_amount = Decimal('0')
+    for title in find(items, needle):
+        for part in items[title]:
+            product_amount += part['amount']
+    return product_amount
 
 
-def expire(items, in_advance_days=0):
-    ...
+print(amount(goods, 'яйца'))
+# Вывод: 1
+print(amount(goods, 'морковь'))
+# Вывод: 5
+
+#def expire(items, in_advance_days=0):
+#    ...
