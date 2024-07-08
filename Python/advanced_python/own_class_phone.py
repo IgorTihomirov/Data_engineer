98d1a1f299c7e8ad1f59a18257ce26d105f962cd
Igor Tikhomirov <i.n.tihomirov>
2024-07-08 20:10:56 +0300

    change file: own_class_phone

--- Python/advanced_python/own_class_phone.py
+++ Python/advanced_python/own_class_phone.py
@@ -1,15 +1,33 @@
 class Phone:
+    # Атрибут класса.
     line_type = 'проводной'
 
+    # Это инициализатор/конструктор класса, в котором объявлено два параметра.
+    def __init__(self, dial_type_value):
+        # Вот он - атрибут объекта. dial_type - атрибут объекта
+        # dial_type_value - значение для атрибута объекта,
+        # которое берется из параметра конструктора.
+        self.dial_type = dial_type_value
 
-rotary_phone = Phone()
-keypad_phone = Phone()
 
-# Печать содержимого атрибута line_type через объект rotary_phone.
+# Создать объект rotary_phone со значением dial_type_value,
+# равным 'дисковый'.
+rotary_phone = Phone(dial_type_value='дисковый')
+# Создать объект keypad_phone со значением dial_type_value,
+# равным 'кнопочный'.
+keypad_phone = Phone(dial_type_value='кнопочный')
+
+# Печать значения атрибута класса.
 print(rotary_phone.line_type)
-# Печать содержимого атрибута line_type через объект keypad_phone.
+# Печать значения атрибута объекта.
+print(rotary_phone.dial_type)
+# Печать значения атрибута класса.
 print(keypad_phone.line_type)
+# Печать значения атрибута объекта.
+print(keypad_phone.dial_type)
 
 # Выведется:
 # проводной
-# проводной 
+# дисковый
+# проводной
+# кнопочный 
