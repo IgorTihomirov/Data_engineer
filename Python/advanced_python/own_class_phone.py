eb64134e6c8d44ce879802a1e5ce8728b610bacf
Igor Tikhomirov <i.n.tihomirov>
2024-07-09 19:08:52 +0300

    change file: Python/advanced_python/own_class_phone.py

--- Python/advanced_python/own_class_phone.py
+++ Python/advanced_python/own_class_phone.py
@@ -25,9 +25,3 @@ print(rotary_phone.dial_type)
 print(keypad_phone.line_type)
 # Печать значения атрибута объекта.
 print(keypad_phone.dial_type)
-
-# Выведется:
-# проводной
-# дисковый
-# проводной
-# кнопочный 
