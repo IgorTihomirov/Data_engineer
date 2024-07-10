4150b1ea9cec4b918f200300550010360fd6c9bf
Igor Tikhomirov <i.n.tihomirov@yandex.ru>
2024-07-10 20:45:47 +0300

    change file: own_class_first_project.py - modified, add inheritance classes

--- Python/advanced_python/own_class_first_project.py
+++ Python/advanced_python/own_class_first_project.py
@@ -10,15 +10,30 @@ class Employee:
         self.second_name = second_name_value
         self.gender = gender_value
         self.remaining_vacation_days = Employee.vacation_days
-    
+
     def consume_vacation(self, free_vacation_days_value):
         self.remaining_vacation_days -= free_vacation_days_value
-        
-    def get_vacation_details(self):
-        return f'Остаток отпускных дней: {self.remaining_vacation_days}'
-      
 
-# Пример использования класса:
-employee = Employee('Роберт', 'Крузо', 'м')
-employee.consume_vacation(7)
-print(employee.get_vacation_details())
+    def get_vacation_details(self):
+        return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
+
+
+class FullTimeEmployee(Employee):
+    def get_unpaid_vacation(self, date_unpaid_vacation, amount_days):
+        self.date_unpaid_vacation = date_unpaid_vacation
+        self.amount_days = amount_days
+        return f'Начало неоплачиваемого отпуска: {str(self.date_unpaid_vacation)}, продолжительность: {self.amount_days} дней.'
+
+
+class PartTimeEmployee(Employee):
+    vacation_days = 14
+
+    def __init__(self, first_name_value, second_name_value, gender_value):
+        super().__init__(first_name_value, second_name_value, gender_value)
+        self.remaining_vacation_days = PartTimeEmployee.vacation_days
+
+
+full_time_employee = FullTimeEmployee('Роберт', 'Крузо', 'м')
+print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
+part_time_employee = PartTimeEmployee('Алёна', 'Пятницкая', 'ж')
+print(part_time_employee.get_vacation_details())
