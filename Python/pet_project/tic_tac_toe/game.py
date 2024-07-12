f287bb78f95f412d583652dc67def049160773af
Igor Tikhomirov <i.n.tihomirov@yandex.ru>
2024-07-13 02:18:39 +0300

    add  wins condition

--- Python/pet_project/tic_tac_toe/game.py
+++ Python/pet_project/tic_tac_toe/game.py
@@ -75,6 +75,13 @@ def main():
         game.make_move(row, column, current_player)
         # Перерисовать поле с учётом сделанного хода.
         game.display()
+        # После каждого хода надо делать проверку на победу и на ничью.
+        if game.check_win(current_player):
+            print(f'Победили {current_player}!')
+            running = False
+        elif game.is_board_full():
+            print('Ничья!')
+            running = False
         # Тернарный оператор, через который реализована смена игроков.
         # Если current_player равен X, то новым значением будет O,
         # иначе — новым значением будет X.
