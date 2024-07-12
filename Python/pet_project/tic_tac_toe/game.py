f27ada583ca160f507c2f1e7c7727def3b0f8157
Igor Tikhomirov <i.n.tihomirov@yandex.ru>
2024-07-13 00:39:16 +0300

    update project

--- Python/pet_project/tic_tac_toe/game.py
+++ Python/pet_project/tic_tac_toe/game.py
@@ -1,27 +1,25 @@
-# Объявить класс.
-class Board:
-    # Инициализировать игровое поле - список списков с пробелами.
-    # Пробелы - это пустые клетки.
-    def __init__(self):
-        self.board = [[' ' for _ in range(5)] for _ in range(3)]
+from inspect import getsource
+from gameparts import Board
 
-    # Метод, который обрабатывает ходы игроков.
-    def make_move(self, row, col, player):
-        self.board[row][col] = player
 
-    # Метод, который отрисовывает игровое поле.
-    def display(self):
-        for row in self.board:
-            print('|'.join(row))
-            print('-' * 9)
-
+# Вот новая функция.   
+def main():
 
 # Создать игровое поле - объект класса Board.
-game = Board()
+    game = Board()
 # Отрисовать поле в терминале.
-game.display()
+    game.display()
 # Разместить на поле символ по указанным координатам - сделать ход.
-game.make_move(1, 2, 'X')
-print('Ход сделан!')
+    game.make_move(1, 2, 'X')
+    print('Ход сделан!')
 # Перерисовать поле с учётом сделанного хода.
-game.display()
+    game.display()
+
+
+# А вот вызов этой функции.
+if __name__ == '__main__':
+    main()
+
+
+game = Board()
+print(getsource(Board)) 
