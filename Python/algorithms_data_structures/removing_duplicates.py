402c16c5556ecdf84586fa7f589343908f9c1787
Igor Tikhomirov <i.n.tihomirov@yandex.ru>
2024-07-31 21:56:07 +0300

    new files: check_element_in list.py removing_duplicates.py

---
+++ Python/algorithms_data_structures/removing_duplicates.py
@@ -0,0 +1,18 @@
+def main():
+    elements_count = int(input())
+    array = list((input().split()))
+    count = 0
+    result = array[:1]
+    for item in range(1, elements_count):
+        if array[item - 1] != array[item]:
+            result.append(array[item])
+        else:
+            count += 1
+    result.extend('_' * count)
+    print(*result)
+
+
+if __name__ == '__main__':
+    # Решение оформлено в функцию, эту функцию надо обязательно вызвать:
+    # Яндекс Контест не сможет вызвать её сам.
+    main()
