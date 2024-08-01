7fb2f1ba9b5f60015c30b1e32dde2ad68dec48c3
Igor Tikhomirov <i.n.tihomirov@yandex.ru>
2024-08-01 20:19:08 +0300

    new file: binary_search.py

--- Python/algorithms_data_structures/removing_duplicates.py
+++ Python/algorithms_data_structures/removing_duplicates.py
@@ -2,7 +2,7 @@ def main():
     elements_count = int(input())
     array = list((input().split()))
     count = 0
-    result = array[:1]
+    result = array[:1]  # копирует список до 1 элемента, не отрезая его
     for item in range(1, elements_count):
         if array[item - 1] != array[item]:
             result.append(array[item])
